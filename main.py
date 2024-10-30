fba5381db213daa68ea7d54f803e784d36e74570
XZeroHakerX <150594778+XZeroHakerX@users.noreply.github.com>
2024-10-30 17:19:19 +0100

    Primer Commit de ISLAND PY.

--- main.py
+++ main.py
@@ -1,36 +1,16 @@
-import sys, pygame
-# Inicializamos pygame
+import pygame
+import constantes
+
 pygame.init()
-# Muestro una ventana de 800x600
-size = 800, 600
+
+size = constantes.ALTO_VENTANA, constantes.ANCHO_VENTANA
+
 screen = pygame.display.set_mode(size)
-# Cambio el título de la ventana
-pygame.display.set_caption("Juego BALL")
-# Inicializamos variables
-width, height = 800, 600
-speed = [1, 1]
-white = 255, 255, 255
-# Crea un objeto imagen y obtengo su rectángulo
-ball = pygame.image.load("ball.png")
-ballrect = ball.get_rect()
-# Comenzamos el bucle del juego
-run=True
-while run:
-	# Espero un tiempo (milisegundos) para que la pelota no vaya muy rápida
-	pygame.time.delay(2)
-	# Capturamos los eventos que se han producido
+
+run = True
+while run == True:
 	for event in pygame.event.get():
-		#Si el evento es salir de la ventana, terminamos
-		if event.type == pygame.QUIT: run = False
-	# Muevo la pelota
-	ballrect = ballrect.move(speed)
-	if ballrect.left < 0 or ballrect.right > width:
-		speed[0] = -speed[0]
-	if ballrect.top < 0 or ballrect.bottom > height:
-		speed[1] = -speed[1]
-	#Pinto el fondo de blanco, dibujo la pelota y actualizo la pantalla
-	screen.fill(white)
-	screen.blit(ball, ballrect)
-	pygame.display.flip()
-# Salgo de pygame
+		if event.type == pygame.QUIT:
+			run = False
+
 pygame.quit()
